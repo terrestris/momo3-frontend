11a0a8df71ce2c5247d1b701a8b41f333868f9e4
arieger <rieger@terrestris.de>
2016-07-20 15:29:33 +0200

    app/model/Rbma.js

--- app/store/Rbma.js
+++ app/store/Rbma.js
@@ -4,37 +4,13 @@ Ext.define('MoMo.client.store.Rbma', {
 
     alias: 'store.rbma',
 
-    //model: 'MoMo.client.model.Rbma',
+    model: 'MoMo.client.model.Rbma',
 
-    mode: 'local',
+//    parentIdProperty: 'parentFolder',
 
     root: {
-        expanded: true,
-        text: "Document root"
-    },
-    data: [{
-        text: "Chapter 1",
-        children: [{
-            text: "Chapter 1.1",
-            leaf: true
-        }, {
-            text: "Chapter 1.2",
-            expanded: true,
-            children: [{
-                text: "Chapter 1.2.1",
-                leaf: true
-            }, {
-                text: "Chapter 1.2.2",
-                leaf: true
-            }]
-        }]
-    }, {
-        text: "Chapter 2",
-        expanded: true,
-        children: [{
-            text: "Chapter 2.1",
-            leaf: true
-        }]
-    }]
-
+        expanded: false,
+        text: "Document root",
+        id: 678
+    }
 });
