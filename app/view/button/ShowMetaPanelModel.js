2c31643f596adf945c1da56f4c38b8ca73f47ff7
arieger <rieger@terrestris.de>
2016-10-18 11:37:31 +0200

    i18n meta info

---
+++ app/view/button/ShowMetaPanelModel.js
@@ -0,0 +1,34 @@
+/* Copyright (c) 2016 terrestris GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * ShowMetaPanelModel
+ *
+ * The view model for the metainformation panel
+ *
+ * @class ShowMetaPanelModel
+ */
+Ext.define('MoMo.client.view.button.ShowMetaPanelModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.button.showmetapanel',
+
+    data: {
+        /*i18n*/
+        tooltip: '',
+        /*i18n*/
+        text: null
+    }
+});
