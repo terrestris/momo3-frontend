6cfb1263d57130b28bdf7f2723686c3cadcfe2fe
mmacata <tawalika@terrestris.de>
2016-11-18 15:30:01 +0100

    added file headers and newlines to multisearch components

--- classic/src/view/container/MultiSearchSettings.js
+++ classic/src/view/container/MultiSearchSettings.js
@@ -1,3 +1,27 @@
+/* Copyright (c) 2016 terrestris GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ *  A container showing settings for the multisearch including switching
+ *  gazetter or wfs search on and off, limit search results to visible extent
+ *  and configure layers for wfs search. Extends the
+ * `BasiGX.view.container.MultiSearchSettings` class.
+ *
+ * @class MoMo.client.view.container.MultiSearchSettings
+ */
 Ext.define('MoMo.client.view.container.MultiSearchSettings',{
     extend: 'BasiGX.view.container.MultiSearchSettings',
     xtype: 'momo-container-multisearchsettings',
